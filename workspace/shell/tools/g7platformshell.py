2eca0948d3ec72505b4441f426609e9912308df6
余洋 <yuyangmx2@gmail.com>
2015-08-07 16:16:09 +0800

    优化shell管理器

--- workspace/shell/tools/g7platformshell.py
+++ workspace/shell/tools/g7platformshell.py
@@ -2,11 +2,13 @@
 __author__ = 'yuyang'
 
 from G7Platform.G7Globals import *
-
-def start_g7platformshell():
-	shellString = django_manage_path+" shell"
+def start_g7platformshell(command="shell"):
+	shellString = django_manage_path+" {command}".format(command=command)
 	os.system(shellString)
 
-
 if __name__ == '__main__':
-	start_g7platformshell()
+	if len(sys.argv) == 2:
+		commandString=sys.argv[1]
+		start_g7platformshell(commandString)
+	else:
+		start_g7platformshell()
